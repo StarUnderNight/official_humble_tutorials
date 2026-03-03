dda11a7bb2f259c1e52170502e89e8e1fcf0dec1
pc <1424249401@qq.com>
2026-03-03 16:51:26 +0800

    save

--- ros_ws/src/02_beginner_client_libraries/py_pubsub/setup.py
+++ ros_ws/src/02_beginner_client_libraries/py_pubsub/setup.py
@@ -24,6 +24,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'talker = py_pubsub.publisher_member_function:main',
+            'listener = py_pubsub.subscriber_member_function:main'
         ],
     },
 )

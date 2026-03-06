0413863bffd6ba65806fcb64b9132b263c9bfd5b
HomePC <1424249401@qq.com>
2026-03-06 21:24:47 +0800

    python版本参数，包括setup.py中添加launch目录

---
+++ ros_ws/src/02_beginner_client_libraries/py_parameters/setup.py
@@ -0,0 +1,34 @@
+import os.path
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = 'py_parameters'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='h',
+    maintainer_email='1424249401@qq.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'minimal_param_node = py_parameters.python_parameters_node:main'
+        ],
+    },
+)
